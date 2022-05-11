2733bb5d3d5b20f525c61c33259b642bbc9ea82f
iamtanmay <iamtanmay@gmail.com>
2022-05-12 01:58:53 +0200

    Update and rename Task.cs to ITask.cs

---
+++ ITask.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pipelines {
+  
+  //Smallest unit of pipelines
+  public interface ITask{
+    
+    //Unique Identifier for type
+    int ID;
+    
+    //Evaluate outcome of task. 0 unfinished, [-1, -2, -3,....-n] failed with parameter, [1, 2, 3...n] pass with parameter
+    //Called OnEvent
+    int Evaluate(string parameter);
+    
+    //Set up environment for task
+    void Initialise();
+  }
+}
